2ea1ac9553942a934915933cc4b28696223aec1f
Julia Saari <mariajulia.saari@gmail.com>
2020-01-22 15:03:00 +0000

    changed Player Class to have an ArrayList

--- Player.java
+++ Player.java
@@ -1,39 +1,38 @@
+import java.util.ArrayList;
 
 public class Player {
 	private String name;
-	private Card[] playerDeck;
+	private ArrayList<Card> playerDeck;
 
 	public Player(String n) {
-		playerDeck = new Card[40];
+		playerDeck = new ArrayList<Card>();
 		name = n;
 	}
 
-	//returns the top card from the player deck
+	// returns the top card from the player deck
 	public Card getTopCard() {
-		return playerDeck[0];
+		Card topcard = playerDeck.get(0);
+		playerDeck.remove(0);
+		return topcard;
 	}
+
+	// adds cards to the bottom of the deck (probably);
+	public void addCards(ArrayList<Card> c) {
+		playerDeck.addAll(c);
+	}
+
 	
-	//method to push the contents of the Array to the top of the deck
-	public void shiftDeck() {
-		for (int i = 0; i<playerDeck.length-1; i++) {
-			playerDeck[i] = playerDeck[i+1]; //setting the card at index i to be the card at the next index 
-			if(playerDeck[i] == null) { //if i is null then we have probably gotten to the end of the deck
-				playerDeck[i-1] = null; // in that case delete the last card - it has already been copied to the prevoius index 
-			}
-		}
+	public Boolean isWinner() {
+		if (playerDeck.get(39) != null) {
+			return true;
+		} else
+			return false;
 	}
 
-	//method to take an Array of Card objects and add them to the bottom of the player deck
-	public void addCards(Card[] d) {
-		int dIndex = 0;
-		while (dIndex < d.length) {
-			for (int i = 0; i < playerDeck.length-1; i++) {
-				if (playerDeck[i] == null) {
-					playerDeck[i] = d[dIndex];
-					dIndex++;
-				}
-			}
-		}
-
+	public Boolean isLoser() {
+		if (playerDeck.get(0) == null) {
+			return true;
+		} else
+			return false;
 	}
 }
