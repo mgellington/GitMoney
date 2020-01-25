76654252e8d95d4d2d3e993caa3aabd96b65d930
Julia Saari <mariajulia.saari@gmail.com>
2020-01-25 14:51:50 +0000

    Edited Player class with new Deck methods

--- Player.java
+++ Player.java
@@ -1,39 +1,31 @@
 import java.util.ArrayList;
 
 public class Player {
-	private String name;
-	protected ArrayList<Card> playerDeck;
+	private int playerNumber;
+	private Deck playerDeck;
 
-	public Player(String n) {
-		playerDeck = new ArrayList<Card>();
-		name = n;
+	public Player(int n) {
+		playerDeck = new Deck();
+		playerNumber = n;
 	}
 	
 	public void setDeck(ArrayList<Card> c) {
 		playerDeck = c;	
 	}
 
-	public ArrayList<Card> getDeck() {
+	public Deck getDeck() {
 		return playerDeck;
-	}
-	
-	// returns the top card from the player deck
-	public Card getTopCard() {
-		Card topcard = playerDeck.get(0);
-		playerDeck.remove(0);
-		return topcard;
 	}
 
 	// adds cards to the bottom of the deck (probably);
-	public void addCards(ArrayList<Card> c) {
-		ArrayList<Card> tempDeck = new ArrayList<Card>();
-		tempDeck = playerDeck;
-		tempDeck.addAll(c);
-		this.setDeck(tempDeck);
+	public void addCards(Deck d) {
+		playerDeck.addSetofCards(d);
+
 	}
 	
+	
 	//method to check if the player is the winner - they have all the cards 
-	public Boolean isWinner() {
+	public Boolean isFull() {
 		if (playerDeck.size() == 40) {
 			return true;
 		} else
@@ -41,10 +33,11 @@ public class Player {
 	}
 	
 	// method to check if the player has lost - if they have 0 cards 
-	public Boolean isLoser() {
+	public Boolean isEmpty() {
 		if (playerDeck.size() == 0) {
 			return true;
 		} else
 			return false;
 	}
 }
+
