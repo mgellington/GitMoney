53f419b90d504d931a9e7681e6cc0e0f617a0b38
Julia Saari <mariajulia.saari@gmail.com>
2020-02-13 19:07:13 +0000

    moved glassgowbars into folder

---
+++ MScIT_TeamProject_TemplateProject/src/online/OCTest.java
@@ -0,0 +1,10 @@
+package online;
+
+public class OCTest {
+	
+	public static void main (String [] args) {
+		OnlineController oController = new OnlineController(4);
+		System.out.println(oController.getRoundInfo());
+	}
+
+}
